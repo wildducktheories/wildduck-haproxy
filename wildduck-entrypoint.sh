3757394012a2cc245c7f81fd38a9353d891c4ced
Jon Seymour <jon@wildducktheories.com>
2016-06-15 13:24:45 +1000

    Ensure the link always gets updated.

--- wildduck-entrypoint.sh
+++ wildduck-entrypoint.sh
@@ -3,18 +3,19 @@
 # Ensure that there is a combined file generated from the letsencrypt key and certificate materials.
 if private=$(readlink -f /etc/letsencrypt/live/wildducktheories.com/privkey.pem) &&
 	archive=$(dirname "$private") &&
+	combined=${archive}/combined.pem
 	test -d "$archive"; then
-	cat $private /etc/letsencrypt/live/wildducktheories.com/fullchain.pem > $archive/combined.pem.tmp
-	if ! test -f $archive/combined.pem || ! cmp $archive/combined.pem.tmp $archive/combined.pem; then
-		test -f $archive/combined.pem && echo "replacing existing combined key - sha1 - $(sha1sum $archive/combined.pem)" 1>&2
-		mv $archive/combined.pem.tmp $archive/combined.pem
-		echo "updated combined key - sha1 - $(sha1sum $archive/combined.pem)" 1>&2
+	cat $private /etc/letsencrypt/live/wildducktheories.com/fullchain.pem > ${combined}.tmp
+	if ! test -f $combined || ! cmp ${combined}.tmp $combined; then
+		test -f $combined && echo "replacing existing combined key - sha1 - $(sha1sum $combined)" 1>&2
+		mv ${combined}.tmp $combined
+		echo "updated combined key - sha1 - $(sha1sum $combined)" 1>&2
 	fi
-	if ! test -L /etc/letsencrypt/live/wildducktheories.com/combined.pem; then
-		ln -sf "$(dirname "$(readlink -f /etc/letsencrypt/live/wildducktheories.com/privkey.pem)")/combined.pem" /etc/letsencrypt/live/wildducktheories.com
+	if test "$(readlink -f /etc/letsencrypt/live/wildducktheories.com/combined.pem)" != "$combined"; then
+		ln -sf "$(dirname "$(readlink /etc/letsencrypt/live/wildducktheories.com/privkey.pem)")/combined.pem" /etc/letsencrypt/live/wildducktheories.com
 	fi
-	if test -f $archive/combined.pem.tmp; then
-		rm $archive/combined.pem.tmp
+	if test -f ${combined}.tmp; then
+		rm ${combined}.tmp
 	fi
 else
 	echo "letsencrypt key material has not been set up yet" 1>&2
